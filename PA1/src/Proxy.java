a38801712857ef53218f98be560f8fa55395367f
Parsa Hejabi <parsa.hejabi@gmail.com>
2019-05-03 02:25:19 +0430

    Proxy class now creates a TCP request and will send it to example.com

--- PA1/src/Proxy.java
+++ PA1/src/Proxy.java
@@ -1,5 +1,35 @@
+import java.io.*;
+import java.net.Socket;
+import java.net.URL;
+
 public class Proxy {
-    public static void main(String[] args) {
-        
+
+    private static final int PORT_NUMBER = 80;
+
+    public static void main(String[] args) throws IOException {
+        URL destinationUrl = new URL("http://example.com/");
+
+        Socket socket = new Socket(destinationUrl.getHost(), PORT_NUMBER);
+
+        OutputStream outputStream = socket.getOutputStream();
+        PrintWriter writer = new PrintWriter(outputStream);
+
+        writer.println("GET " + destinationUrl.getPath() + " HTTP/1.1");
+        writer.println("Host: " + destinationUrl.getHost());
+        writer.println("Accept: text/html");
+        writer.println("Connection: close");
+        writer.println();
+
+        writer.flush();
+
+        InputStream inputStream = socket.getInputStream();
+        BufferedReader reader = new BufferedReader(new InputStreamReader(inputStream));
+
+        String line;
+
+        while ((line = reader.readLine()) != null){
+            System.out.println(line);
+        }
+        socket.close();
     }
 }
